bed4d6496092639def985c9dc8f728512b7bf04e
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2018-01-10 00:14:47 -0800

    Update Logic.cs

--- Pong_game/Logic.cs
+++ Pong_game/Logic.cs
@@ -8,6 +8,9 @@ class Logic : UI
 
     // Clock to compute the changes on screen if any
     Timer animate = new Timer();
+
+    Timer test = new Timer();
+
     public Logic()
     {
         
@@ -17,12 +20,16 @@ class Logic : UI
         animate.Tick += Animate_Tick;
         animate.Interval = 10;
 
+        test.Tick += test_Tick;
+        test.Interval = 50;
+
         start.Click += Start_Click;
         reset.Click += Reset_Click;
 
         // Need this to detect inputs
         this.KeyPreview = true;
-        (game_area as Control).KeyPress += Button_Control;
+        (game_area as Control).KeyDown += Button_Control_Down;
+        (game_area as Control).KeyUp += Button_Control_Up;
 
         // Prevent the flickering
         typeof(Panel).InvokeMember("DoubleBuffered",
@@ -34,16 +41,58 @@ class Logic : UI
     // Redraw the canvas
     private void Update_Tick(object sender, EventArgs e)
     {
+
         game_area.Invalidate();
     }
 
     // Update every tick.interval
     private void Animate_Tick(object sender, EventArgs e)
     {
-        pluck.location.X += pluck.speed;
-        pluck.location.Y += pluck.speed;
+
+        pluck.location.X += (int)pluck.speedX;
+        pluck.location.Y += (int)pluck.speedY;
+
+        if (paddle1.moveup_keypress && paddle1.location.Y > game_area.Top)
+        {
+            paddle1.location.Y -= paddle1.speed;
+        }
+        else if (paddle1.movedown_keypress && paddle1.location.Y + paddle1.height < game_area.Bottom)
+        {
+            paddle1.location.Y += paddle1.speed;
+        }
+
+        if (paddle2.moveup_keypress && paddle2.location.Y > game_area.Top)
+        {
+            paddle2.location.Y -= paddle2.speed;
+        }else if (paddle2.movedown_keypress && paddle2.location.Y + paddle2.height < game_area.Bottom)
+        {
+            paddle2.location.Y += paddle2.speed;
+        }
+
+        if(pluck.location.X < game_area.Left || pluck.location.X + pluck.diameter > game_area.Right)
+        {
+            pluck.speedX = -pluck.speedX;
+        }else if(pluck.location.Y < game_area.Top || pluck.location.Y + pluck.diameter > game_area.Bottom)
+        {
+            pluck.speedY = -pluck.speedY;
+        }
+
+        Console.WriteLine(pluck.speedX);
+        Console.WriteLine(pluck.speedY);
+        
+        /*
+        Console.WriteLine(paddle1.moveup_keypress);
+        Console.WriteLine(paddle1.movedown_keypress);
+        Console.WriteLine(paddle2.moveup_keypress);
+        Console.WriteLine(paddle2.movedown_keypress);
+        */
     }
 
+    private void test_Tick(object sender, EventArgs e)
+    {
+        pluck.speedX *= 1.01;
+        pluck.speedY *= 1.01;
+    }
     private void Start_Click(object sender, EventArgs e)
     {
         // Set focus to game_area so key inputs will work
@@ -52,6 +101,13 @@ class Logic : UI
         // Start clocks
         update.Start();
         animate.Start();
+        //test.Start();
+        Console.WriteLine(game_area.Top);
+        Console.WriteLine(game_area.Bottom);
+        Console.WriteLine(game_area.Height);
+        Console.WriteLine(this.Height);
+        Console.WriteLine(this.ClientSize.Height);
+
     }
 
     private void Reset_Click(object sender, EventArgs e)
@@ -62,32 +118,53 @@ class Logic : UI
         // Stop both clocks
         update.Stop();
         animate.Stop();
-
+        test.Stop();
         // Redraw the UI
         game_area.Invalidate();
     }
 
     // Handle key inputs
-    private void Button_Control(object sender, KeyPressEventArgs e)
+    private void Button_Control_Down(object sender, KeyEventArgs e)
     {
-        switch (e.KeyChar)
+        switch (e.KeyCode)
         {
-            case (char)Keys.W:
-                paddle1.location.Y -= paddle1.speed;
+            case Keys.W:
+                paddle1.moveup_keypress = true;
                 break;
 
-            case (char)Keys.S:
-                paddle1.location.Y += paddle1.speed;
+            case Keys.S:
+                paddle1.movedown_keypress = true;
                 break;
 
-            case (char)Keys.Up:
-                paddle2.location.Y -= paddle2.speed;
+            case Keys.Up:
+                paddle2.moveup_keypress = true;
                 break;
 
-            case (char)Keys.Down:
-                paddle2.location.Y += paddle2.speed;
+            case Keys.Down:
+                paddle2.movedown_keypress = true;
+                break;
+        }
+    }
+
+    private void Button_Control_Up(object sender, KeyEventArgs e)
+    {
+        switch (e.KeyCode)
+        {
+            case Keys.W:
+                paddle1.moveup_keypress = false;
                 break;
 
+            case Keys.S:
+                paddle1.movedown_keypress = false;
+                break;
+
+            case Keys.Up:
+                paddle2.moveup_keypress = false;
+                break;
+
+            case Keys.Down:
+                paddle2.movedown_keypress = false;
+                break;
         }
     }
 
