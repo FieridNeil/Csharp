49514780a022edb523887782e47610981117dd81
Fierid Neil <14002607+FieridNeil@users.noreply.github.com>
2018-01-10 20:22:09 -0800

    Finished

--- Pong_game/Logic.cs
+++ Pong_game/Logic.cs
@@ -9,19 +9,21 @@ class Logic : UI
     // Clock to compute the changes on screen if any
     Timer animate = new Timer();
 
-    Timer test = new Timer();
+    // Clock to increase the speed of the pluck over time
+    Timer timer = new Timer();
 
+    
     public Logic()
     {
         
         update.Tick += Update_Tick;
-        update.Interval = 3;
+        update.Interval = 10;
 
         animate.Tick += Animate_Tick;
         animate.Interval = 10;
 
-        test.Tick += test_Tick;
-        test.Interval = 50;
+        timer.Tick += timer_Tick;
+        timer.Interval = 50;
 
         start.Click += Start_Click;
         reset.Click += Reset_Click;
@@ -41,7 +43,6 @@ class Logic : UI
     // Redraw the canvas
     private void Update_Tick(object sender, EventArgs e)
     {
-
         game_area.Invalidate();
     }
 
@@ -52,6 +53,7 @@ class Logic : UI
         pluck.location.X += (int)pluck.speedX;
         pluck.location.Y += (int)pluck.speedY;
 
+        // Handle paddle collision
         if (paddle1.moveup_keypress && paddle1.location.Y > game_area.Top)
         {
             paddle1.location.Y -= paddle1.speed;
@@ -69,30 +71,57 @@ class Logic : UI
             paddle2.location.Y += paddle2.speed;
         }
 
-        if(pluck.location.X < game_area.Left || pluck.location.X + pluck.diameter > game_area.Right)
+
+        // Handle pluck collision
+        if (pluck.location.X < paddle1.location.X + paddle1.width && pluck.location.Y > paddle1.location.Y && pluck.location.Y < paddle1.location.Y + paddle1.height)
         {
             pluck.speedX = -pluck.speedX;
-        }else if(pluck.location.Y < game_area.Top || pluck.location.Y + pluck.diameter > game_area.Bottom)
+        }
+        if(pluck.location.X + pluck.diameter > paddle2.location.X && pluck.location.Y > paddle2.location.Y && pluck.location.Y < paddle2.location.Y + paddle2.height)
+        {
+            pluck.speedX = -pluck.speedX;
+        }
+        // If the pluck hits the left wall => player 2 wins
+        if (pluck.location.X < game_area.Left)
+        {
+            game_reset();
+            Console.WriteLine("Player 2 wins");
+        }
+
+        // If the pluck hits the right wall => player 1 wins;
+        if (pluck.location.X + pluck.diameter > game_area.Right)
+        {
+            game_reset();
+            Console.WriteLine("Player 1 wins");
+        }
+        if(pluck.location.Y < game_area.Top || pluck.location.Y + pluck.diameter > game_area.Bottom)
         {
             pluck.speedY = -pluck.speedY;
         }
-
-        Console.WriteLine(pluck.speedX);
-        Console.WriteLine(pluck.speedY);
         
-        /*
-        Console.WriteLine(paddle1.moveup_keypress);
-        Console.WriteLine(paddle1.movedown_keypress);
-        Console.WriteLine(paddle2.moveup_keypress);
-        Console.WriteLine(paddle2.movedown_keypress);
-        */
     }
 
-    private void test_Tick(object sender, EventArgs e)
+    // Increase the speed of the pluck over time
+    private void timer_Tick(object sender, EventArgs e)
     {
-        pluck.speedX *= 1.01;
-        pluck.speedY *= 1.01;
+        pluck.speedX *= 1.001;
+        pluck.speedY *= 1.001;
     }
+
+    // Reset the game if the reset button is clicked or when either player wins
+    private void game_reset()
+    {
+        // Reset the game UI
+        GamePlayUI();
+
+        // Stop both clocks
+        update.Stop();
+        animate.Stop();
+        timer.Stop();
+        // Redraw the UI
+        game_area.Invalidate();
+    }
+
     private void Start_Click(object sender, EventArgs e)
     {
         // Set focus to game_area so key inputs will work
@@ -101,26 +130,13 @@ class Logic : UI
         // Start clocks
         update.Start();
         animate.Start();
-        //test.Start();
-        Console.WriteLine(game_area.Top);
-        Console.WriteLine(game_area.Bottom);
-        Console.WriteLine(game_area.Height);
-        Console.WriteLine(this.Height);
-        Console.WriteLine(this.ClientSize.Height);
+        timer.Start();
 
     }
 
     private void Reset_Click(object sender, EventArgs e)
     {
-        // Reset the game UI
-        GamePlayUI();
-
-        // Stop both clocks
-        update.Stop();
-        animate.Stop();
-        test.Stop();
-        // Redraw the UI
-        game_area.Invalidate();
+        game_reset();
     }
 
     // Handle key inputs
@@ -146,6 +162,7 @@ class Logic : UI
         }
     }
 
+    // Handle key inputs
     private void Button_Control_Up(object sender, KeyEventArgs e)
     {
         switch (e.KeyCode)
